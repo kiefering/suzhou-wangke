c5c307fb5b4ee85a39c5693830df8e469f132a0b
Kiefer <kiefering@gmail.com>
2022-02-22 13:41:40 +0800

    Add files via upload

--- content_script.js
+++ content_script.js
@@ -15,12 +15,7 @@ $('.crawl-btn').click(function(){
     imgBox.append(imgWrap);
   })
   console.log(imgArr)
-  chrome.storage.sync.set({'imgArr': imgArr}, function() {
-    // Notify that we saved.
-    console.log('保存成功');
-    // chrome.pageAction.setBadgeText({text: '新增'});
-    // chrome.pageAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
-  });
+  
 
   Modal.show({
     title: '提取结果',
@@ -74,19 +69,8 @@ $('.crawl-btn').click(function(){
 // $('img')[0].src = imgURL;
 
 chrome.runtime.onMessage.addListener(
-  function(message, sender, sendResponse) {
-    if (message == "activeBtn"){
-      if(!$('.crawl-btn')) {
-        $('body').append("<div class='crawl-btn'>提取</div>")
-      }else {
-        $('.crawl-btn').css("background-color","orange");
-        setTimeout(() => {
-          $('.crawl-btn').css("background-color","#06c");
-        }, 3000);
-      }
-      sendResponse({farewell: "激活成功"});
-    }
- });
+  
+});
 
 //  chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
 //   console.log(response, document.body);
